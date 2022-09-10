832bbfed7f33c8663ec1048c97441cdec957b4b9
siva <sivashakthiarun.c@gmail.com>
2022-09-10 14:15:13 +0530

    operators

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// Arithmetic operators
+
+
+var a=5;
+var b=2;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+a++;
+console.log(a);
+b--;
+console.log(b)
+console.log(++a);
+console.log(--b);
+console.log("");
+
+// Assignment operators
+
+var c=10;
+var d=5;
+console.log(c=d);
+console.log(c+=d);
+console.log(c-=d);
+console.log(c*=d);
+console.log(c/=d);
+console.log(c%=d);
+console.log(c**=d);
+console.log("");
+
+// Comparison operators
+var e=10
+var f=20;
+console.log(e<f);
+console.log(5>6);
+console.log(e<=f);
+console.log(e>=f);
+console.log(e==f);
+console.log(e!=f);
+console.log(e!==f);
+console.log(10===10 ? "siva": "ajay")
+
+
+//Logical operator
+
+var siva=10;
+
+if(siva>11 && siva>9){
+    console.log("shakthi");
+}
+else{
+    console.log("Wikki");
+}
+
+console.log(Math.pow(2,3))
